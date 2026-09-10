6bcbc80d77075e2eaa60740d2396718535b80a8c
chanwoo <chanwoolee74@gmail.com>
2026-09-10 13:00:52 +0900

    style: Feedbacker 오타 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: {
-    default: 'Feedbaker',
-    template: '%s | Feedbaker', // 페이지별 title을 설정할때 사용되는 템플릿
+    default: 'Feedbacker',
+    template: '%s | Feedbacker', // 페이지별 title을 설정할때 사용되는 템플릿
   },
   description:
     '만든 서비스를 홍보하고, 실제 사용자에게 피드백과 테스트를 받는 플랫폼입니다.',
   metadataBase: new URL('http://localhost:3000'), // 추후 배포 시 도메인 주소로 변경 필요
   openGraph: {
-    title: 'Feedbaker',
+    title: 'Feedbacker',
     description:
       '만든 서비스를 홍보하고, 실제 사용자에게 피드백과 테스트를 받는 플랫폼입니다.',
     type: 'website',
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
         url: '/og_image.png', // 카카오톡 링크 공유 시 표시되는 이미지
         width: 1200,
         height: 630,
-        alt: 'Feedbaker',
+        alt: 'Feedbacker',
       },
     ],
   },
